d4902415a69fe57e4a5ea92101d547e8bb578f6b
Felipe Goiabeira <felipe.goiabeira@ceuma.br>
2021-04-07 23:50:08 -0300

    Add Spring Initializr files

---
+++ src/test/java/com/multi/tenant/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.tenant.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
